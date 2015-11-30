d6e6b0a36233e1f0c177c89c8b88efcdc1331617
Jayin Ton <tonjayin@gmail.com>
2015-12-01 02:20:27 +0800

    支持登陆修改

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,7 +1,10 @@
+'use strict';
 import React from 'react';
 import $ from 'jquery';
 
 require('./App.css');
+const APP_ID = '7hyoc6are05n823dd424ywvf752gem2w96inlkl3yiann6vw';
+const APP_KEY = 'tgufkdybjtb4gvsbwcatiwd9wx49adxrmf8qkpwunh0h3wx3'
 
 export default class App extends React.Component {
 
@@ -9,7 +12,8 @@ export default class App extends React.Component {
         super(props);
         this.state = {
             results: [],
-            page: 1
+            page: 1,
+            sessionToken: ''
         };
     }
 
@@ -23,8 +27,8 @@ export default class App extends React.Component {
             type: "GET",
             url: url.replace('{skip}', (page-1)*limit).replace('{limit}', limit),
             headers: {
-                'X-LC-Id': '7hyoc6are05n823dd424ywvf752gem2w96inlkl3yiann6vw',
-                'X-LC-Key': 'tgufkdybjtb4gvsbwcatiwd9wx49adxrmf8qkpwunh0h3wx3'
+                'X-LC-Id': APP_ID,
+                'X-LC-Key': APP_KEY
             },
             processData: false,
             success: function(data) {
@@ -48,11 +52,70 @@ export default class App extends React.Component {
     nextPage() {
         this.fetchDate(this.state.page+1);
     }
+    /**
+     * 登陆
+     * @return {[type]} [description]
+     */
+    clickLogin() {
+        let url = 'https://leancloud.cn:443/1.1/login?username={username}&password={password}'
+
+        $.ajax({
+            type: "GET",
+            url: url.replace('{username}', 'admin').replace('{password}', 'jayinton'),
+            headers: {
+                'X-LC-Id': APP_ID,
+                'X-LC-Key': APP_KEY
+            },
+            processData: false,
+            success: function(data) {
+                this.setState({
+                    sessionToken: data.sessionToken
+                })
+
+            }.bind(this)
+        });
+    }
+    /**
+     * 选择
+     * @param  {string} item 一条记录
+     * @param  {[type]} evt  时间
+     * @return {[type]}      [description]
+     */
+    handleSelectChange(item, evt) {
+        console.log(item)
+        console.log(evt.target.value)
+
+        let url = 'https://leancloud.cn:443/1.1/classes/FeedBack/{objectId}'
+        let data = {
+            status: evt.target.value
+        }
+
+        $.ajax({
+            type: "PUT",
+            url: url.replace('{objectId}', item.objectId),
+            headers: {
+                'X-LC-Id': APP_ID,
+                'X-LC-Key': APP_KEY,
+                'X-LC-Session': this.state.sessionToken,
+                'Content-Type': 'application/json'
+            },
+            data: JSON.stringify(data),
+            success: function(data) {
+                this.fetchDate(this.state.page)
+                alert('修改成功!')
+            }.bind(this),
+            error: function(err, code){
+                console.log(err)
+            }
+        });
+
+    }
 
     render () {
         return(
             <div className="app-container">
                 <h1>编程助手用户反馈跟踪 <small>v0.1</small></h1>
+                <button className="btn btn-primary" onClick={this.clickLogin.bind(this)}>登陆</button>
                 <table className="table table-bordered table-hover">
                     <thead>
                         <tr>
@@ -66,6 +129,7 @@ export default class App extends React.Component {
                     </thead>
                     <tbody>
                         {this.state.results.map(function(item) {
+                            let statusItem = item.status;
                             let statusLabel = 'label label-success';
                             if(item.status === 'close'){
                                 statusLabel = 'label label-danger';
@@ -73,20 +137,28 @@ export default class App extends React.Component {
                             if(item.status === 'pedding'){
                                 statusLabel = 'label label-warning';
                             }
-
+                            if( this.state.sessionToken !== '' ){
+                                statusItem = (
+                                    <select className="form-control" onChange={this.handleSelectChange.bind(this, item)}>
+                                        <option value="open">open</option>
+                                        <option value="close">close</option>
+                                        <option value="pedding">pedding</option>
+                                    </select>
+                                )
+                            }
                             return (
                                 <tr key={item.objectId}>
                                     <td >{item.type === 'report'
                                             ? '反馈'
                                             : "建议"}</td>
-                                    <td className={statusLabel}>{item.status}</td>
+                                    <td className={statusLabel}>{statusItem}</td>
                                     <td >{item.app_version}</td>
                                     <td >{item.system_version}</td>
                                     <td >{item.model}</td>
                                     <td alt={item.content}>{item.content.substring(0, 20)}</td>
                                 </tr>
                             )
-                        })}
+                        }.bind(this))}
                     </tbody>
                 </table>
                 <nav>
